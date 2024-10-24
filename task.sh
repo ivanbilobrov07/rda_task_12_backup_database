256c16ef2808fe447551a83e4f5790f8e6ccbfd0
Ivan Bilobrov <ivanbilobrov07@gmail.com>
2024-10-25 00:00:51 +0300

    done

--- task.sh
+++ task.sh
@@ -1,1 +1,14 @@
 #! /bin/bash
+
+DB_USER=${DB_USER}
+DB_PASSWORD=${DB_PASSWORD}
+
+PROD="ShopDB"
+RESERVE="ShopDBReserve"
+STAGE="ShopDBDevelopment"
+
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD" --no-create-db "$PROD" | \
+mysql -u "$DB_USER" -p"$DB_PASSWORD" "$RESERVE"
+
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD" --no-create-db --no-create-info "$PROD" | \
+mysql -u "$DB_USER" -p"$DB_PASSWORD" "$STAGE"
